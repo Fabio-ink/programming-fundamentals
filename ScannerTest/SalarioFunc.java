792ce51ec8728f7fa3829f99c8833769bc8838ca
Fabio <fabiopradaa@gmail.com>
2025-06-14 13:15:49 -0300

    Exercicios da Udemy feitos no dia 14/06/2025

---
+++ ScannerTest/SalarioFunc.java
@@ -0,0 +1,24 @@
+package ScannerTest;
+import java.util.Scanner;
+
+public class SalarioFunc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Qual o seu nome?");
+        String nome = sc.nextLine();
+
+        System.out.println("Quantas horas você trabalha por mês?");
+        Integer horasTrabalhadas = sc.nextInt();
+
+        System.out.println("E quanto vocẽ ganha por cada hora trabalhada?");
+        Double salarioHora = sc.nextDouble();
+
+        Double salario = horasTrabalhadas * salarioHora;
+
+        System.out.printf("Prazer %s, você trabalha por %s horas ao mês e ganha %s por cada hora trabalhada, logo seu salário é de: R$ %.2f \n", nome, horasTrabalhadas, salarioHora, salario);
+
+        sc.close();
+    }
+
+}

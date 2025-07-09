eab11aab079abc2ce41114c8b7a4efd0f52b2720
Fabio <fabiopradaa@gmail.com>
2025-07-09 20:06:38 -0300

    Exercicios de vetores

---
+++ vectorTest/entities/Person.java
@@ -0,0 +1,41 @@
+package vectorTest.entities;
+
+public class Person {
+
+    private String name;
+    private Double height;
+    private Integer age;
+    
+    public Person(String name, Double height, Integer age) {
+        this.name = name;
+        this.height = height;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    
+
+}

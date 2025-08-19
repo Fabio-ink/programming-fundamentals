3985f98d0b5d568406809a16e37de239b86f0dc9
Fabio <fabio.pradaa@gmail.com>
2025-08-19 18:15:23 -0300

    Forma enxuta de utilizar os leitores do arquivo

--- basics/scanner/files/application/FileReaderBufferedReader.java
+++ basics/scanner/files/application/FileReaderBufferedReader.java
@@ -8,16 +8,11 @@ public class FileReaderBufferedReader {
     public static void main(String[] args) {
         
         String path = "/home/fluxyfp/Área de trabalho/in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try{
-            fr = new FileReader(path);
-            br = new BufferedReader(fr); 
-            //Primeiro instancia FileReader para ler o arquivo em path e depois instancia um BufferedReader para acelerar o processo
-            //É possivel fazer tudo em uma unica linha também se quiser "br = new BufferedReader(new FileReader(path));"
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+          
             String line = br.readLine();
+            
             while (line != null) {
                 System.out.println(line);
                 line = br.readLine();
@@ -25,19 +20,6 @@ public class FileReaderBufferedReader {
         }
         catch (IOException e){
             System.out.println("Error: " + e.getMessage());
-        }
-        finally{
-            try{
-                if(br != null){
-                br.close();
-            }
-            if(fr != null){
-                br.close();
-            }
-            }
-            catch (IOException e){
-                e.printStackTrace();
-            }
         }
 
     }
